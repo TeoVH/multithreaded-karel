99aa8c124ecd9cb9f0ab9f12ccf4ce06ca94f0b7
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-05-05 21:20:39 -0500

    Update - Racer class to run in threads

--- Trabajos/Trains.java
+++ Trabajos/Trains.java
@@ -1,10 +1,31 @@
 import kareltherobot.*;
 import java.awt.Color;
 
+class Racer extends Robot { 
+    public Racer(int Street, int Avenue, Direction direction, int beeps) {
+        super(Street, Avenue, direction, beeps);
+        World.setupThread(this);
+    }
+
+    public void race() {
+        while(!nextToABeeper())
+            move();
+        pickBeeper();
+        turnOff();
+    }
+
+    public void run() {
+        race();
+    }
+}
+
 public class Trains implements Directions {
     public static void main(String [] args) {
         // Read the world Metro Medellín
         World.readWorld("MetroMed.kwld");
         World.setVisible(true);
+
+        Racer first = new Racer(35, 1, West, 0);
+        Racer second = new Racer(35, 2, West, 0);
     }
 }

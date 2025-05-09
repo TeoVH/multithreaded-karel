8331f05370ab7d34612f409ffcf3329a39b8df31
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-05-08 22:00:54 -0500

    Update - Create 32 trains in respective threads in workshop

--- Trabajos/Main.java
+++ Trabajos/Main.java
@@ -1,34 +1,79 @@
 import kareltherobot.*;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    // World configuration
+    static {
         World.readWorld("MetroMedellin.kwld");
         World.setVisible(true);
-        World.setDelay(10);
+        World.setDelay(1);
+    }
 
-        MetroMedellin.Tren trenNiquia = new MetroMedellin.Tren(32, 15, Directions.East, 0, "Niquia", Color.RED);
-        MetroMedellin.Tren trenEstrella = new MetroMedellin.Tren(32, 14, Directions.East, 0, "Estrella", Color.BLUE);
-        MetroMedellin.Tren trenSanJavier = new MetroMedellin.Tren(33, 14, Directions.South, 0, "SanJavier", Color.GREEN);
+    public static void main(String[] args) {
 
-        Thread hiloNiquia = new Thread(trenNiquia);
-        Thread hiloEstrella = new Thread(trenEstrella);
-        Thread hiloSanJavier = new Thread(trenSanJavier);
+        List<Thread> hilos = new ArrayList<>();
+        int trenId = 1;
+        
+        // 7 trenes a Niquia
+        hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 32, 15, Directions.East, 0, Color.RED, "Niquia")));
 
-        hiloNiquia.start();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 3; i++) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 32 + i, 14, Directions.South, 0, Color.RED, "Niquia")));
         }
 
-        hiloEstrella.start();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 3; i++) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, (11 + i), Directions.West, 0, Color.RED, "Niquia")));
         }
 
-        hiloSanJavier.start();
+        // 15 trenes a Estrella
+        for (int i = 0; i < 10; i++) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, 1 + i, Directions.East, 0, Color.BLUE, "Estrella")));
+        }
+
+        for (int i = 0; i < 5; i++) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 35, 1 + i, Directions.West, 0, Color.BLUE, "Estrella")));
+        }
+
+        // 10 trenes a San Javier
+        for (int i = 0; i < 10; i++) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 35, 6 + i, Directions.West, 0, Color.GREEN, "SanJavier")));
+        }
+
+        // for (Thread hilo : hilos) {
+        //     hilo.start();
+        //     try {
+        //         Thread.sleep(500);
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+        
+        
+
+        // MetroMedellin.Tren trenNiquia = new MetroMedellin.Tren(35, 12, Directions.West, 0, Color.RED, "Estrella");
+        // MetroMedellin.Tren trenEstrella = new MetroMedellin.Tren(34, 1, Directions.East, 0, Color.BLUE, "Estrella");
+        // MetroMedellin.Tren trenSanJavier = new MetroMedellin.Tren(34, 7, Directions.East, 0, Color.GREEN, "Estrella");
+
+        // Thread hiloNiquia = new Thread(trenNiquia);
+        // Thread hiloEstrella = new Thread(trenEstrella);
+        // Thread hiloSanJavier = new Thread(trenSanJavier);
+
+        // hiloNiquia.start();
+        // try {
+        //     Thread.sleep(1000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        // hiloEstrella.start();
+        // try {
+        //     Thread.sleep(1000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        // hiloSanJavier.start();
     }
 }
--- Trabajos/MetroMedellin.java
+++ Trabajos/MetroMedellin.java
@@ -6,9 +6,11 @@ public class MetroMedellin implements Directions {
     public static class Tren extends Robot implements Runnable {
         private String destino;
         private Color color;
+        private int trenId;
 
-        public Tren(int street, int avenue, Direction direction, int beeps, Color color, String destino) {
+        public Tren(int trenId, int street, int avenue, Direction direction, int beeps, Color color, String destino) {
             super(street, avenue, direction, beeps, color);
+            this.trenId = trenId;
             this.destino = destino;
             this.color = color;
             World.setupThread(this);
@@ -21,7 +23,7 @@ public class MetroMedellin implements Directions {
             // Lógica de rutas delegada a clase Rutas
             if (destino.equals("Niquia")) {
                 Rutas.IrANiquia(this);
-                Rutas.rutaNiquia(this);
+                // Rutas.rutaNiquia(this);
             } else if (destino.equals("Estrella")) {
                 Rutas.IrAEstrella(this);
                 Rutas.rutaEstrella(this);
@@ -57,35 +59,9 @@ public class MetroMedellin implements Directions {
                 }
             }
         }
-    }
 
-    // World configuration
-    static {
-        World.readWorld("MetroMedellin.kwld");
-        World.setVisible(true);
-        World.setDelay(10);
-    }
-
-    public static void main(String[] args) {
-
-        Tren trenNiquia = new Tren(34, 6, East, 0, Color.red, "Niquia");
-        Tren trenEstrella = new Tren(32, 14, East, 0, Color.blue, "Estrella");
-        Tren trenSanJavier = new Tren(33, 14, South, 0, Color.green, "SanJavier");
-
-        Thread hiloNiquia = new Thread(trenNiquia);
-        Thread hiloEstrella = new Thread(trenEstrella);
-        Thread hiloSanJavier = new Thread(trenSanJavier);
-
-        hiloNiquia.start();
-        try {
-            Thread.sleep(1000); 
-        } catch (InterruptedException e){}
-        
-        hiloEstrella.start();
-        try { 
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {}
-        
-        hiloSanJavier.start();
+        public int getTrenId() {
+            return trenId;
+        }
     }
 }

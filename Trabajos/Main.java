eb46bd0b190b4725e076f68fab2926ec734a314f
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-05-09 01:35:09 -0500

    Fix - All the trains be able to leave workshop

--- Trabajos/Main.java
+++ Trabajos/Main.java
@@ -8,7 +8,7 @@ public class Main {
     static {
         World.readWorld("MetroMedellin.kwld");
         World.setVisible(true);
-        World.setDelay(3);
+        World.setDelay(5);
     }
 
     public static void main(String[] args) {
@@ -17,29 +17,34 @@ public class Main {
         int trenId = 1;
         
         // 7 trenes a Niquia
-        hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 32, 15, Directions.West, 0, Color.RED, "Niquia")));
+        hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 32, 15, Directions.East, 0, Color.RED, "Niquia")));
 
         for (int i = 0; i < 3; i++) {
             hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 32 + i, 14, Directions.South, 0, Color.RED, "Niquia")));
         }
 
-        for (int i = 0; i < 3; i++) {
-            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, (11 + i), Directions.East, 0, Color.RED, "Niquia")));
+        for (int i = 3; i > 0; i--) {
+            hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, 10+i, Directions.East, 0, Color.RED, "Niquia")));
         }
 
         // 15 trenes a Estrella
-        for (int i = 0; i < 10; i++) {
+        for (int i = 9; i > 0; i--) {
             hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, 1 + i, Directions.East, 0, Color.BLUE, "Estrella")));
         }
+
+        hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 34, 1, Directions.South, 0, Color.BLUE, "Estrella")));
 
         for (int i = 0; i < 5; i++) {
             hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 35, 1 + i, Directions.West, 0, Color.BLUE, "Estrella")));
         }
 
         // 10 trenes a San Javier
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 9; i++) {
             hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 35, 6 + i, Directions.West, 0, Color.GREEN, "SanJavier")));
         }
+
+        hilos.add(new Thread(new MetroMedellin.Tren(trenId++, 35, 15, Directions.North, 0, Color.GREEN, "SanJavier")));
+
 
         for (Thread hilo : hilos) {
             hilo.start();
